c00b3879c5e22ad6b778bb9c043a36333813d8cf
Anushree Jha <anushreejha0604@gmail.com>
2024-04-25 18:02:46 +0530

    next1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Box from './components/Box';
+import VerticalDivider from './components/VerticalDivider';
 
 function App() {
   return (
     <div className="page">
       <Box id="box1" />
+      <VerticalDivider />
       <Box id="box2" />
     </div>
   );
---
+++ src/components/VerticalDivider.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../App.css'; 
+
+function VerticalDivider() {
+  return <div className="vertical-divider"></div>;
+}
+
+export default VerticalDivider;
