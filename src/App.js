67e5f1561b4e0cba3ccedb2df066de17290ef612
Anushree Jha <anushreejha0604@gmail.com>
2024-04-25 17:42:24 +0530

    basic boxes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Box from './components/Box';
+
+function App() {
+  return (
+    <div className="page">
+      <Box id="box1" />
+      <Box id="box2" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Box.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import '../App.css'; 
+
+function Box({ id }) {
+  const backgroundImage = id === 'box1' ? 'img/grill.png' : 'img/grill.png';
+
+  return (
+    <div className="box" id={id} style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+  );
+}
+
+export default Box;
