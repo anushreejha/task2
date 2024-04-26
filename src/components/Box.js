80b19e8c531ba193cb5081cf118cf726882a9dc4
Anushree Jha <anushreejha0604@gmail.com>
2024-04-26 16:12:40 +0530

    styles comp

--- src/components/Box.js
+++ src/components/Box.js
@@ -7,9 +7,20 @@ function Box({ id }) {
   return (
     <div className="box" id={id} style={{ backgroundImage: `url(${backgroundImage})` }}>
       {id === 'box1' && (
-        <div className="scroll">
-          <div className="scroll1"></div>
-          <div className="scroll2"></div>
+        <div>
+          <div className="scroll1">
+            <img src="img/1.png" alt="Top Image" />
+            <img src="img/2.png" alt="Bottom Image" />
+            <div className="scroll-handle-1"></div>
+            <div className="scroll-handle-2"></div>
+            <div className="scroll-handle-circle"></div>
+          </div>
+          <div className="scroll2">
+            <img src="img/1.png" alt="Top Image" />
+            <img src="img/2.png" alt="Bottom Image" />
+            <div className="scroll-handle-3"></div>
+              <div className="lights-text">LIGHTS</div>
+          </div>
         </div>
       )}
     </div>
